a141649c717eaf78140f25c45649566fb35952fd
rmarchino <rmarchino95@gmail.com>
2024-06-03 13:15:50 -0500

    Agregando header

--- includes/header.php
+++ includes/header.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Introducción a PHP - Código Con Juan</title>
+    <title>Introducción a PHP </title>
     <link rel="stylesheet" href="css/style.css">
 </head>
 
@@ -13,8 +13,7 @@
 
     <div class="contenido">
         <h1>Desarrollo Web Completo</h1>
-        <a href="http://www.codigoconjuan.com" target="_blank">codigoconjuan.com</a>
         <p>Introducción a Programación Orientada a Objetos en PHP</p>
-        <p class="hashtag">#DesarrolloWebCompleto</p>
+        <p class="hashtag">Desarrollo Web Completo</p>
 
         <div class="resultado">
